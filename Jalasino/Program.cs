257d1a0c12ec38235da482aa6d192862ef65d405
Mahdi Akbari <mahdim2f4@gmail.com>
2024-09-08 13:19:56 +0330

    Add EnsureCreated function on startup

--- Jalasino/Program.cs
+++ Jalasino/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
 namespace Jalasino
 {
     internal static class Program
@@ -11,6 +13,8 @@ namespace Jalasino
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            DatabaseFacade facade = new DatabaseFacade(new DataContex());
+            facade.EnsureCreated();
             Application.Run(new Form1());
         }
 
