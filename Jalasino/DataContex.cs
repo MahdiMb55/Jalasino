8135934203512b4bc082faa688102b3a12857d20
Mahdi Akbari <mahdim2f4@gmail.com>
2024-09-08 13:15:33 +0330

    Add project files.

---
+++ Jalasino/DataContex.cs
@@ -0,0 +1,20 @@
+﻿using Jalasino.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalasino
+{
+    public class DataContex:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = UserData.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Jalasino/Form1.cs
@@ -0,0 +1,10 @@
+namespace Jalasino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
