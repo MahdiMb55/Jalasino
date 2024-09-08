cc29ddcfb658758f56a714a6a586f24f14d15ac2
Mahdi Akbari <mahdim2f4@gmail.com>
2024-09-08 15:38:23 +0330

    Add Krypton Toolkit package

--- Jalasino/Form1.Designer.cs
+++ Jalasino/Form1.Designer.cs
@@ -29,11 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.kryptonManager1 = new Krypton.Toolkit.KryptonManager(this.components);
+            this.kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            this.SuspendLayout();
+            // 
+            // kryptonManager1
+            // 
+            this.kryptonManager1.GlobalPaletteMode = Krypton.Toolkit.PaletteMode.Microsoft365BlackDarkMode;
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(331, 43);
+            this.kryptonButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(202, 60);
+            this.kryptonButton1.TabIndex = 0;
+            this.kryptonButton1.Values.Text = "مدیریت جلسات";
+            this.kryptonButton1.Click += new System.EventHandler(this.kryptonButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(826, 358);
+            this.Controls.Add(this.kryptonButton1);
+            this.Font = new System.Drawing.Font("Vazir Medium UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Jalasino";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Krypton.Toolkit.KryptonManager kryptonManager1;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
     }
 }
--- Jalasino/Form1.cs
+++ Jalasino/Form1.cs
@@ -1,10 +1,25 @@
+using Krypton.Toolkit;
 namespace Jalasino
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Krypton.Toolkit.KryptonForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //using (DataContex contex=new DataContex())
+            //{
+            //    contex.Users.Add(new Models.User() { Name="",Role=""});
+            //    contex.SaveChanges();
+            //}
+        }
+
+        private void kryptonButton1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
