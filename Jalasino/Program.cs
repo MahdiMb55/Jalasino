0166484f134fc409007320cc243872637a338ac0
Mahdi Akbari <mahdim2f4@gmail.com>
2024-09-11 02:27:26 +0330

    Add ebtity .tools

--- Jalasino/Program.cs
+++ Jalasino/Program.cs
@@ -10,11 +10,9 @@ namespace Jalasino
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
             DatabaseFacade facade = new DatabaseFacade(new DataContex());
             facade.EnsureCreated();
+            ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
 
